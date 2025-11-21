96df356c62b1bc940a4901f4d78107a1808a1442
Angelica Garrido <42750416+garridoangelica@users.noreply.github.com>
2025-11-21 13:33:23 -0500

    Committing 4 items from workspace dfeef225-5614-4404-b47a-3fbaecefac22

---
+++ Fabric/Workspaces/DataEngineeringWSDevCICD/SilverToGoldNotebook.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "11d5a2f9-aac4-4120-8c3d-71415bcc85d5",
+# META       "default_lakehouse_name": "SilverLakehouse",
+# META       "default_lakehouse_workspace_id": "dfeef225-5614-4404-b47a-3fbaecefac22",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "11d5a2f9-aac4-4120-8c3d-71415bcc85d5"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
